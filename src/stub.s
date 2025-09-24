4021b1dd86cb82175680c192da464c1fa6b8dc5f
masaru ogawa <masaruo@gmail.com>
2025-09-24 19:02:20 +0900

    stub.s restored from AI messsing up

--- src/stub.s
+++ src/stub.s
@@ -3,6 +3,14 @@ default rel
 
 %include "./src/rc4.s"
 
+; registers rolls
+; r12 = base_address of ASLR
+; r11 = executable_runtime_address
+; rcx = executable_size
+; r10 = OEP
+; r9 = payload_start_address
+; r8w = decrypt key value
+
 ; define seciton for offsets
 %define executable_addr 0
 %define executable_size 8
@@ -14,107 +22,80 @@ default rel
 global _start
 
 section .bss
-    state: resb 256 ; memory for RC4 State-Box
+	state: resb 256 ; memory for RC4 State-Box
 
 section .text
 
 _start:
-    push rbp
-    mov rbp, rsp
+	push rbp
+	mov rbp, rsp
+	lea    r12, [rel _start]                ; runtime address of the stub
+	lea    r9, [rel _start + STUB_SIZE]    ; r9 points to payload
+	mov    rax, [r9 + stub_vaddr]            ; stub's virtual addr
+	sub r12, rax                        ; ASLR's base_addr = runtime address - virtual addr
 
-    ; === DEBUG: Print "START" to see if we get here ===
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_start]
-    mov rdx, 6
-    syscall
-    ; ================================================
-
-    lea r12, [rel _start]               ; runtime address of the stub
-    lea r9, [rel _start + STUB_SIZE]    ; r9 points to payload
-
-    ; === DEBUG: Now test payload access ===
-    mov rax, [r9 + stub_vaddr]        ; stub's virtual addr
-    sub r12, rax                      ; ASLR's base_addr = runtime address - virtual addr
-    ; ======================================
-
-    ; === DEBUG: Print "MIDDLE" ===
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_middle]
-    mov rdx, 7
-    syscall
-    ; ============================
-
-    ; === NOW ADD BACK THE DECRYPTION ===
 prepare_decrypt:
-    ; === DEBUG: Test payload data access ===
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_prepare]
-    mov rdx, 8
-    syscall
-
-    ; Test each payload access individually
-    mov r11, [r9 + executable_addr]     ; vaddr of executable section
-
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_size]
-    mov rdx, 5
-    syscall
-
-    mov rcx, [r9 + executable_size]
-
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_add]
-    mov rdx, 4
-    syscall
-
-    add r11, r12                        ; runtime addr of executable section
-    ; ===================================================
+	mov    r11, [r9 + executable_addr]        ; vaddr of executable section
+	mov rcx, [r9 + executable_size]
+	add    r11, r12                        ; runtime addr of executable section (base_addr + vaddr)
 
 decrypt:
-    ; === DEBUG: Skip RC4 for now ===
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_init]
-    mov rdx, 5
-    syscall
-    ; ===============================
-    ; =====================================
+	; init_state
+	lea rdi, [rel state]
+	mov rsi, 256
+	call init_state
+
+	; key scheduling algorithm
+	lea rdi, [r9 + key_offset]
+	lea rsi, [rel state]
+	mov rdx, 16
+	call ksa
+
+	; rc4
+	lea rdi, [rel state]
+	mov rsi, r11
+	mov rdx, rcx
+	call rc4
+
+;decrypt_loop:
+	;xor    byte[r11], 0xff
+	;inc    r11
+	;dec rcx
+	;jnz    decrypt_loop
 
 goto_OEP:
-    ; === DEBUG: Now test OEP access ===
-    mov r10, [r9 + original_entry_point]  ; OEP vaddr
-    add r10, r12                           ; actual OEP = vaddr + base_addr
-    ; ==================================
+	mov    r10, [r9 + original_entry_point]        ; OEP vaddr
+	add    r10, r12                                ; actual OEP = vaddr + base_addr
+	call greeting
+	mov    rsp, rbp
+	pop	rbp
+	and    rsp, -16                                ; stack alignment
+	jmp r10                                    ; jmp to OEP
 
-    ; === DEBUG: Print before jump ===
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel debug_end]
-    mov rdx, 4
-    syscall
-    ; ===============================
+; --- Function to print a greeting ---
+greeting:
+	push rax
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push r11
 
-    ; === DEBUG: Now try the actual jump to OEP ===
-    leave           ; restore stack frame
-    and rsp, -16    ; stack alignment
-    jmp r10         ; jump to OEP - THIS IS WHERE IT PROBABLY SEGFAULTS
-    ; ============================================
+	mov    rax, 1                            ; WRITE
+	mov    rdi, 1                            ; STDOUT
+	lea    rsi, [rel greetings_data]        ; STR
+	mov    rdx, 14                            ; SIZE
+	syscall
 
-debug_start: db "START", 10
-debug_middle: db "MIDDLE", 10
-debug_end: db "END", 10
-debug_zero: db "ZERO", 10
-debug_big: db "BIG", 10
-debug_init: db "INIT", 10
-debug_ksa: db "KSA", 10
-debug_rc4: db "RC4", 10
-debug_prepare: db "PREPARE", 10
-debug_size: db "SIZE", 10
-debug_add: db "ADD", 10
+	pop r11
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	pop rax
+	ret
+
+; --- Data used by the greeting function ---
+greetings_data: db "....WOODY....", 10; 10 = NL
 
 end_of_stub:
